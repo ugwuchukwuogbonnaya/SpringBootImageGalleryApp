c1792949ed96a66538fa36671f5713f41486117d
ugwuchukwuogbonnaya <stcasmir4excel@gmail.com>
2022-05-31 19:19:21 +0100

    first commit

---
+++ src/main/java/com/casmir/org/repository/ImageGalleryRepository.java
@@ -0,0 +1,9 @@
+package com.casmir.org.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.casmir.org.entity.ImageGallery;
+
+public interface ImageGalleryRepository extends JpaRepository<ImageGallery, Long> {
+
+}
---
+++ src/main/java/com/casmir/org/service/ImageGalleryService.java
@@ -0,0 +1,30 @@
+package com.casmir.org.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.casmir.org.entity.ImageGallery;
+import com.casmir.org.repository.ImageGalleryRepository;
+
+@Service
+public class ImageGalleryService {
+
+	@Autowired
+	private ImageGalleryRepository imageGalleryRepository;
+	
+	public void saveImage(ImageGallery imageGallery) {
+		imageGalleryRepository.save(imageGallery);	
+	}
+
+	public List<ImageGallery> getAllActiveImages() {
+		return imageGalleryRepository.findAll();
+	}
+
+	public Optional<ImageGallery> getImageById(Long id) {
+		return imageGalleryRepository.findById(id);
+	}
+}
+
